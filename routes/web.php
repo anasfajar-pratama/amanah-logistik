a6e35f8eb44478f4e1366f72a073e3a97e0f1f25
anasfajar <simpirnimil@gmail.com>
2026-06-15 22:29:33 +0700

    awalun

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get('/admin/{any?}', function () {
 Route::get('/{any?}', function () {
     return view('app');
 })->where('any', '^(?!api|admin).*$')->name('landing');
+
+Route::get('/storage/{path}', function (string $path) {
+    $filePath = storage_path('app/public/' . $path);
+    if (!file_exists($filePath)) {
+        abort(404);
+    }
+    $mime = mime_content_type($filePath);
+    return response()->file($filePath, ['Content-Type' => $mime]);
+})->where('path', '.*');
